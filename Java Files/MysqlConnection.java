c79c5c41125d9f546e14eff576dc327cd732fa11
Ebon <Ebonbmooremartin@gmail.com>
2016-11-14 21:36:34 -0500

    structure change/added login work in progress

--- Java Files/MysqlConnection.java
+++ Java Files/MysqlConnection.java
@@ -1,18 +1,26 @@
 //STEP 1. Import required packages
 import java.sql.*;
+import java.util.Scanner
 
 public class MysqlConnection {
    // JDBC driver name and database URL
    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-   static final String DB_URL = "jdbc:mysql://localhost:3307/csci490";
+   static final String DB_URL = "jdbc:mysql://localhost:3306/csci490";
 
-   //  Database credentials
-   static final String USER = "root";
-   static final String PASS = "Spiral117";
    
    public static void main(String[] args) {
+   
    Connection conn = null;
    Statement stmt = null;
+   
+   //database credentials
+   Scanner in = new Scanner(System.in);
+   System.out.println("User: ");
+   static final String USER = in.nextLine();
+   System.out.println("Pass: ");
+   static final String PASS = in.nextLine();
+   
+   
    try{
       //STEP 2: Register JDBC driver
       Class.forName("com.mysql.jdbc.Driver");
@@ -39,6 +47,7 @@ public class MysqlConnection {
          System.out.print("LotId: " + id);
          System.out.print(", spotId: " + spotid);
          System.out.print(", spot type: " + spotT);
+        
          
       }
       //STEP 6: Clean-up environment
